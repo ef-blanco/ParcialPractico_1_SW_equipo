3ac701a54dde7853869a9539a9087996b80b8c6d
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2025-03-07 14:16:57 -0500

    Pesistencia Terminada

--- src/main/java/co/edu/uniandes/dse/parcial1/entities/RutaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/RutaEntity.java
@@ -1,7 +1,12 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
@@ -10,5 +15,9 @@ public class RutaEntity extends BaseEntity {
     private String nombre;
     private String color;
     private String tipo;
+
+    @PodamExclude
+    @ManyToMany
+    private List<EstacionEntity> estaciones = new ArrayList<>();
 
 }
